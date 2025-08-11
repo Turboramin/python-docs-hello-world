8da7fbd5cde56d12feb53dd51bb61a43ec3c7f37
Ramin <43754521+Turboramin@users.noreply.github.com>
2025-08-11 22:55:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return 'PoC by Ramin Topfer (<a href="https://www.linkedin.com/in/ramintopfer/" target="_blank">LinkedIn</a>)'
+    return 'Subdomain takeover by Ramin Topfer (<a href="https://www.linkedin.com/in/ramintopfer/" target="_blank">LinkedIn</a>)'
     
